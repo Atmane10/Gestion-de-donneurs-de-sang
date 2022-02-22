2d6ebdfe3db8fa72cb365ea31bb377ca3ad39ed6
AbidarAtmane <abidaratmane@gmail.com>
2022-02-22 09:49:12 +0100

    ''

--- src/Login.java
+++ src/Login.java
@@ -43,7 +43,7 @@ public class Login extends JFrame {
 	public Login() {
 		setTitle("Gestion des donneurs de sang");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 551, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
